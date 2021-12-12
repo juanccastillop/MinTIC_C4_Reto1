e3462087fc8d0735b36d6f5a29a398ef055d0e88
Juan Pedreros <Juan.Pedreros@ilumno.com>
2021-12-12 11:05:47 -0500

    Front y Back completo reto1

--- src/main/java/co/edu/usa/lasartenreto1/reto1_lasarten/controllers/UserController.java
+++ src/main/java/co/edu/usa/lasartenreto1/reto1_lasarten/controllers/UserController.java
@@ -15,31 +15,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.usa.lasartenreto1.reto1_lasarten.models.User;
 import co.edu.usa.lasartenreto1.reto1_lasarten.services.UserService;
-
+/**
+* Anotaciones e Inicio clase principal y marcador service
+*/ 
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin("*")
 public class UserController {
-    
+    /**
+    * Anotacion de Autowired para inyeccion de dependencias
+    */ 
     @Autowired
     private UserService userService;
-    
+    /**
+    * Metodo get all
+    */ 
     @GetMapping("/all")
     public List<User> getAll() {
         return userService.getAll();
     }
-    
+    /**
+    * Metodo post para guardar usuario
+    */ 
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
     public User registrar(@RequestBody User user) {
         return userService.registrar(user);
     }
-    
+    /**
+    * Metodo get por medio de email y password
+    */ 
     @GetMapping("/{email}/{password}")
     public User autenticarUsuario(@PathVariable("email") String email, @PathVariable("password") String password) {
         return userService.autenticarUsuario(email, password);
     }
-    
+    /**
+    * metodo get por email
+    */ 
     @GetMapping("/{email}")
     public boolean existeEmail(@PathVariable("email") String email) {
         return userService.existeEmail(email);
